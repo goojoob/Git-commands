3d553db28c5eb0e7497fd5b118ed760eade4cdce
Antonio Rodríguez <antrorc@gmail.com>
2021-11-05 11:40:03 +0100

    initial commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Antonio Rodriguez Cia on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(carouselDatas: mocExample)
+        }
+    }
+}
